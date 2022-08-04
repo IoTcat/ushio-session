43d95bd09af9d5cf43bf92cc63e6d40530ffa100
iotcat <i@iotcat.me>
2022-08-03 21:47:19 -0400

    pm2 to node

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var expressWs = require('express-ws');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require(__dirname+'/routes/index');
 //var usersRouter = require('./routes/users');
 var app = express();
 
